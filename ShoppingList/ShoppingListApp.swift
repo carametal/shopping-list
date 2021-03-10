d08529bd970f6edfa6a8e68dbcfbacf08cbfb7ab
carametal <mkatsdevelop@gmail.com>
2021-03-10 19:20:27 +0900

    Initial commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Atsushi Maki on 2021/03/10.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
